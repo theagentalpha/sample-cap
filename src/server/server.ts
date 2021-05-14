ad8f7f4e059c34d90ebeb22fc94005585ec80051
GDub <none@none.com>
2021-05-14 09:51:17 -0400

    SSL

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
 import apiRouter from './routes';
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/home/ec2-user/project/example.key'),
+    cert: fs.readFileSync('/home/ec2-user/project/example.crt'),
+  }, app);
 
 app.use(express.static('public'));
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+httpsServer.listen(port, () => console.log(`Server listening on port: ${port}`));
